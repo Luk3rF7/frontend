787389595cd5c311c325fb42b7ce3358be9d403c
luk3rF7 <lkzeron@gmail.com>
2023-07-07 20:31:12 -0300

    drumkit musical

---
+++ JavaScrip_miniProjetos/Drumkit/index.js
@@ -0,0 +1,56 @@
+'use strich';/* pega erro de ma praticas */
+/*  json */
+const sons ={
+    'B' : 'boom.wav',
+    'C' : 'clap.wav',
+    'H' : 'hihat.wav',
+    'K' : 'kick.wav',
+    'O' : 'openhat.wav',
+    'R' : 'ride.wav',
+    'S' : 'snare.wav',
+    'T' : 'tink.wav',
+    'M' : 'tom.wav',
+    'V' : 'tom.wav',
+}
+/* json */
+const criarDiv = (texto) => {
+    const div = document.createElement('div');
+    div.classList.add('key');
+    div.textContent = texto;
+    div.id =texto;
+    document.getElementById('container').appendChild(div);
+}
+
+const exibir = (sons) => Object.keys(sons).forEach(criarDiv);
+
+const tocarSom = (letra) => {
+const audio = new Audio( `./sounds/${sons[letra]}`);
+audio.play();
+}
+const adicionarEfeito = (letra) => document.getElementById(letra)
+                                           .classList.toggle('active');
+const removerEfeito = (letra) => {
+    const div =document.getElementById(letra);
+    const removeActive = () => div.classList.remove('active');
+    div.addEventListener('transitionend',removeActive);
+}
+const ativarDiv = (evento) =>{
+    
+    const letra = evento.type == 'click' ? evento.target.id : evento.key.toUpperCase();
+  
+    const letraPermitida = sons.hasOwnProperty(letra);
+    if(letraPermitida){
+        adicionarEfeito(letra);
+          tocarSom(letra);
+          removerEfeito(letra);
+    }
+  
+}
+
+exibir(sons);
+document.getElementById('container')
+        .addEventListener('click', ativarDiv);
+
+ window.addEventListener('keyup',ativarDiv);
+
+
